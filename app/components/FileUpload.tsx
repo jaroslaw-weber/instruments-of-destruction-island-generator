04ff1602e80770b80b112aca65d7fc72a8be0a80
Jaroslaw Weber <jaroslawweber@gmail.com>
2024-06-28 23:01:54 +0900

    fix file upload

--- app/components/FileUpload.tsx
+++ app/components/FileUpload.tsx
@@ -6,11 +6,11 @@ import { uniq } from "lodash";
 function FileUpload({}) {
   const [_structures, setStructures] = useAtom(structuresAtom);
   const [_island, setIsland] = useAtom(islandAtom);
-  const handleFileUpload = (event) => {
+  const handleFileUpload = (event:any) => {
     const file = event.target.files[0];
     const reader = new FileReader();
     reader.onload = (e) => {
-      const island = JSON.parse(e.target.result);
+      const island = JSON.parse(e.target?.result as string);
       const structures: Structure[] = island.m_structures;
       setStructures(structures);
       setIsland(island);
